c3fd1f2bf4c2864c1ae40d192f8089a70b6df5df
KarthikNageswaran <karthick.information@gmail.com>
2020-09-04 14:09:15 +0530

    adding new test

---
+++ src/test/java/jenkinstest/TestngTest2.java
@@ -0,0 +1,12 @@
+package jenkinstest;
+
+import org.testng.annotations.Test;
+
+public class TestngTest2 {
+	
+	@Test
+	public void testing2(){
+		System.out.println("Application Testing testcase 2 !!!");
+	}
+
+}
