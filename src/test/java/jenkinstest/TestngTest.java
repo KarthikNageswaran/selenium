eda4481eb4282159dc2be6a5f7d1216616a104b9
KarthikNageswaran <karthick.information@gmail.com>
2020-09-04 13:02:46 +0530

    first commit

---
+++ src/test/java/jenkinstest/TestngTest.java
@@ -0,0 +1,12 @@
+package jenkinstest;
+
+import org.testng.annotations.Test;
+
+public class TestngTest {
+	
+	@Test
+	public void testing(){
+		System.out.println("Application Testing !!!");
+	}
+
+}
